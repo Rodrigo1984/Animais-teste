859c13f83a288af9cd6a140497d34b102fe72198
Rodrigo <rodriregialves@gmail.com>
2023-08-31 15:37:29 -0300

    scroll suave transformado em classe

--- js/modules/sroll-suave.js
+++ js/modules/sroll-suave.js
@@ -1,60 +1,32 @@
-export default function initScrollSuave(){
+export default class ScrollSuave {
+  constructor(links, options) {
+    this.linksInternos = document.querySelectorAll(links);
+    if (options === undefined) {
+      this.options = { behavior: 'smooth', block: 'start' };
+    } else {
+      this.options = options;
+    }
 
-
-    const menuItems = document.querySelectorAll('[data-menu="suave"] a[href^="#"]');
-  
-    menuItems.forEach(item => {
-      item.addEventListener('click', scrollToIdOnClick);
-    })
-  
-    function getScrollTopByHref(element) {
-      const id = element.getAttribute('href');
-      return document.querySelector(id).offsetTop;
-    }
-  
-    function scrollToIdOnClick(event) {
-      event.preventDefault();
-      const to = getScrollTopByHref(event.target) - 80;
-      scrollToPosition(to);
-    }
-  
-    function scrollToPosition(to) {
-      // window.scroll({
-      //   top: to,
-      //   behavior: "smooth",
-      // });
-      smoothScrollTo(0, to);
-    }
-  
-    /**
-     * Smooth scroll animation
-     * @param {int} endX: destination x coordinate
-     * @param {int} endY: destination y coordinate
-     * @param {int} duration: animation duration in ms
-     */
-    function smoothScrollTo(endX, endY, duration) {
-      const startX = window.scrollX || window.pageXOffset;
-      const startY = window.scrollY || window.pageYOffset;
-      const distanceX = endX - startX;
-      const distanceY = endY - startY;
-      const startTime = new Date().getTime();
-  
-      duration = typeof duration !== 'undefined' ? duration : 1000;
-  
-      // Easing function
-      const easeInOutQuart = (time, from, distance, duration) => {
-        if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
-        return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
-      };
-  
-      const timer = setInterval(() => {
-        const time = new Date().getTime() - startTime;
-        const newX = easeInOutQuart(time, startX, distanceX, duration);
-        const newY = easeInOutQuart(time, startY, distanceY, duration);
-        if (time >= duration) {
-          clearInterval(timer);
-        }
-        window.scroll(newX, newY);
-      }, 1000 / 60); // 60 fps
-    }
+    this.scrollToSection = this.scrollToSection.bind(this);
   }
+
+  scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    section.scrollIntoView(this.options);
+  }
+
+  addLinkEvent() {
+    this.linksInternos.forEach((link) => {
+      link.addEventListener('click', this.scrollToSection);
+    });
+  }
+
+  init() {
+    if (this.linksInternos.length) {
+      this.addLinkEvent();
+    }
+    return this;
+  }
+}
